bb4f8e4db2d99d96f70d3c58e86feb96bd041575
Nathan Bogan <bogannathan@gmail.com>
2017-08-16 13:12:18 -0400

    added datatypes

---
+++ 0.01_datatypes.js
@@ -0,0 +1,47 @@
+// data types
+
+// strings
+
+	console.log("What a Wonderful World".length) 
+	console.log('Hello you guys'.toUpperCase())//for example can modify user data, so yES becomes YES and Yes becomes YES so that it can be processed correctly
+	"it's a wonderful day"; //can get around using single quotes for apostrophes by double quoting around
+	'Thomas said "I think grass is green!"' //vice versa
+	"it\'s a wonderful day" //backslash omits the function of the next character
+
+	console.log("Nathan Bogan".toLowerCase())
+	console.log('jay Jackson'[0].toUpperCase() + 'jay'[1] + 'jay'[2])
+
+	console.log('Home'[0] + 'Home'[1] + 'Home'[2].toUpperCase() + 'Home'[3])
+	console.log(typeof("Amber"))
+
+// numbers
+
+	console.log(typeof(18))
+	console.log(17%13)
+
+	console.log(10)
+
+// booleans
+	console.log(typeof(true))
+
+/*	conditional operators are often used in booleans
+	> less than
+	< greater than
+	= equal
+	>= less than or equal to
+	<= greater than or equal to
+	== equal to
+	=== strictly equal to
+	! not
+	!= not equal to
+*/
+
+	console.log(4==6)
+	console.log("test".length > "ego".length)
+	console.log(1=="1") //is value one present on the other side? Yes, true. even though not data type number, the string returns does return 1. 
+	console.log(1==="1")
+	console.log(1=="1 and")
+	console.log('\n')
+		// console.log('\n')
+		// 	console.log('\n')
+		// 		console.log('\n')
