dfb52512cccddaf7e76eff269c88fdb75635ad81
Nathan Bogan <bogannathan@gmail.com>
2017-08-23 11:03:48 -0400

    added query selector

---
+++ 0.10_document_querySelector.js
@@ -0,0 +1,31 @@
+// document.querySelector(id or class)
+
+// grabs the element that has that class or id
+
+var el = document.querySelector(".myclass");
+
+var el = document.querySelector("div.user-panel.main input[name='login']");
+
+//elements in el can be accessed with an index
+
+
+// <h2>JavaScript HTML DOM!</h2>
+
+// <p>Hello World!</p>
+
+// <p>Hello Norway!</p>
+
+// <p id="demo"></p>
+
+// <script>
+// var myNodelist = document.querySelectorAll("p");
+// document.getElementById("demo").innerHTML =
+// "The innerHTML of the second paragraph is: " +
+// myNodelist[1].innerHTML;
+// </script>
+
+// </body>
+// </html>
+
+
+// jsDOM
