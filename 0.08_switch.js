3cc12aff33640f6fd3a13a5a6c8cbc6f425ef034
Nathan Bogan <bogannathan@gmail.com>
2017-08-29 12:49:46 -0400

    updated switch

--- 0.08_switch.js
+++ 0.08_switch.js
@@ -1,16 +1,20 @@
 // switch is a function that changes a variable to something else if it meets certain criteria. break ends the function. 
 let player = "Paul George"
-switch	(player) {
-	case 'Lebron James':
-		console.log("Lebron is the second best ever");
-		break;
-	case 'Michael Jordan':
-		console.log("the GOAT");
-		break;
-	case 'Paul George':
-		console.log("PG-13 gave up on the pacers");
-		break;
-	default:
-		console.log("I don't know who that is");
+function tellMe(player) {
+	switch	(player) {
+		case 'Lebron James':
+			console.log("Lebron is the second best ever");
+			break;
+		case 'Michael Jordan':
+			console.log("the GOAT");
+			break;
+		case 'Paul George':
+			console.log("PG-13 gave up on the pacers");
+			break;
+		default:
+			console.log("I don't know who that is");
+	}
 }
-
+tellMe(player)
+player = "Michael Jordan"
+tellMe(player)
